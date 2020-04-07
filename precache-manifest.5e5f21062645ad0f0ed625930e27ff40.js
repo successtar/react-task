0ea754ee194d2f2ba3f3f1e0df29ad1bd75a3aa9
Hammed Osanyinpeju <osanyinpejuhammed35@gmail.com>
2020-04-07 15:16:43 +0100

    Updates

---
+++ precache-manifest.5e5f21062645ad0f0ed625930e27ff40.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ceec5bdca7f57180b2b76062c6a26745",
+    "url": "/react-task/index.html"
+  },
+  {
+    "revision": "acf53cba9b3a27f250fd",
+    "url": "/react-task/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "d682353c6c74ed1c542b",
+    "url": "/react-task/static/js/2.79e76750.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-task/static/js/2.79e76750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "acf53cba9b3a27f250fd",
+    "url": "/react-task/static/js/main.998ea2bf.chunk.js"
+  },
+  {
+    "revision": "4c397da0ffea5e59cf3f",
+    "url": "/react-task/static/js/runtime-main.31a2c056.js"
+  },
+  {
+    "revision": "8f85f711494c3c3a13800147819e8a5c",
+    "url": "/react-task/static/media/profile.8f85f711.PNG"
+  }
+]);
